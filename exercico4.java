c578cd9d326f62524c0b57f20e83c610933459e2
Igor Vinoski <62020437+IgorVinoski@users.noreply.github.com>
2022-06-09 23:42:15 +0000

    none

--- exercico4.java
+++ exercico4.java
@@ -7,13 +7,6 @@ informados não podem formar um triângulo”. Para que três valores de
 lados formem um triângulo, cada par de lados somados não pode ser
 menor ou igual ao terceiro lado..
 
- um de seus lados deve ser maior que o valor absoluto (módulo) da diferença dos outros dois lados e menor que a soma dos outros dois lados.
-
-
-
-
-
-Só irá existir um triângulo se, somente se, os seus lados obedeceram à seguinte regra: um de seus lados deve ser maior que o valor absoluto (módulo) da diferença dos outros dois lados e menor que a soma dos outros dois lados. Veja o resumo da regra abaixo: Não pare agora.
 */
   public static void main(String[] args) {
     int ladoa, ladob, ladoc;
