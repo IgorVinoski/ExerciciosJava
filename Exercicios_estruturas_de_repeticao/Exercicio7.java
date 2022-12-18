4b0def3245c8e826da22c2ebdca825c77557fc9a
unknown <igor.vinoski2@gmail.com>
2022-12-18 17:12:18 -0300

    Exercicio7

---
+++ Exercicios_estruturas_de_repeticao/Exercicio7.java
@@ -0,0 +1,13 @@
+package Exercicios_estruturas_de_repeticao;
+
+public class Exercicio7 {
+    public static void main(String[] args){
+        /*Faça um programa que apresente os quadrados dos números inteiros de
+15 a 200.*/
+        int i = 15;
+        while(i<200){
+            System.out.println(i*i);
+            i++;
+        }
+    }
+}
